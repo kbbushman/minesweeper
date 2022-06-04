f33b298f36527ef1078a1dfd26ffeeff270f7c64
kbbushman <k.bushman@yahoo.com>
2022-06-04 15:03:02 -0700

    Adds scoreboard

---
+++ src/components/Scoreboard/Counter.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export interface CounterProps {
+  /**
+   * Number displayed in the counter
+   */
+  children: string;
+}
+
+export const Counter: React.FC<CounterProps> = ({ children }) => {
+  return <Frame>{children}</Frame>;
+};
+
+const Frame = styled.div`
+  display: inline-block;
+  padding: 0 0.3vw;
+  color: #ec433c;
+  border: 0.15vw inset;
+  line-height: 2vw;
+  letter-spacing: 0.08em;
+  background: #333;
+  text-shadow: 0 0 0.1vw #ec433c;
+`;
